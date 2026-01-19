f9e06ab2bcd252d94dfdc57c98f4472518ed27bf
niklasdemmrich <niklas.demmrich@tum.de>
2026-01-19 09:10:24 +0100

    laurenz is sweet

---
+++ src/preprocessing.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+import pandas as pd
+
+BASE_DIR = Path(__file__).resolve().parent.parent  # project root
+df = pd.read_csv(BASE_DIR / "data" / "all_accelerometer_data_pids_13.csv")
+
+print(df)
