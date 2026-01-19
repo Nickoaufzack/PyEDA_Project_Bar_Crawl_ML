a28ea2e3523f902a479140ee483991f368ff37df
niklasdemmrich <niklas.demmrich@tum.de>
2026-01-19 12:39:44 +0100

    laurenz is sweet

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,7 +4,7 @@ import openpyxl
 
 BASE_DIR = Path(__file__).resolve().parent.parent  # project root
 df = pd.read_csv(BASE_DIR / "data" / "all_accelerometer_data_pids_13.csv")
-df_a = pd.read_excel(BASE_DIR / "data" / "raw_tac"/ "CC6740 CAM Results.xlsx")
+df_a = pd.read_csv(BASE_DIR / "data" / "clean_tac"/ "BK7610_clean_TAC.csv")
 
 print(df_a)
 
